47f63d4fee0d35be8a79d8bc7250c7a28c03919c
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-01-08 11:18:17 -0300

    add port error handler

--- srcs/ircserv.cpp
+++ srcs/ircserv.cpp
@@ -2,19 +2,21 @@
 
 int main(int argc, char **argv)
 {
+    Server ser;
     if (argc != 3){
         std::cerr << "Uso: " << argv[0] << " <porta> <senha>" << std::endl; 
         return (1);
     }
-    Server ser;
     introduce();
     try {
-        signal(SIGINT, Server::SignalHandler); //-> captura o sinal (ctrl + c)
-        signal(SIGQUIT, Server::SignalHandler); //-> captura o sinal (ctrl + \)
-        ser.ServerInit(std::atoi(argv[1]), argv[2]); //-> inicializa o servidor
+        signal(SIGINT, Server::SignalHandler);
+        signal(SIGQUIT, Server::SignalHandler);
+        if (std::atoi(argv[1] < 1024))
+            throw(std::runtime_error("Porta inválida"));
+        ser.ServerInit(std::atoi(argv[1]), argv[2]);
     }
     catch (const std::exception& e){
-        ser.CloseFds(); //-> fecha os descritores de arquivo
+        ser.CloseFds();
         std::cerr << e.what() << std::endl;
     }
     std::cout << "O Servidor Foi Fechado!" << std::endl;
