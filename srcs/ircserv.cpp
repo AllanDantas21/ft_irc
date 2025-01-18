14061df39e75857e05baba06a858c48de01f8d8f
allandantas21 <allandantas21.ad@gmail.com>
2025-01-18 19:24:03 -0300

    finish refacts

--- srcs/ircserv.cpp
+++ srcs/ircserv.cpp
@@ -1,23 +1,23 @@
 #include "../incs/ircserv.hpp"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     Server ser;
-    if (argc != 3){
+    if (argc != 3) {
         std::cerr << "Uso: " << argv[0] << " <porta> <senha>" << std::endl; 
-        return (1);
+        return 1;
     }
     introduce();
     try {
         signal(SIGINT, Server::SignalHandler);
         signal(SIGQUIT, Server::SignalHandler);
-        if (std::atoi(argv[1]) < 1024)
-            throw(std::runtime_error("Porta inválida"));
+        if (std::atoi(argv[1]) < 1024) {
+            throw std::runtime_error("Porta inválida");
+        }
         ser.ServerInit(std::atoi(argv[1]), argv[2]);
-    }
-    catch (const std::exception& e){
+    } catch (const std::exception& e) {
         ser.CloseFds();
         std::cerr << e.what() << std::endl;
     }
     std::cout << "O Servidor Foi Fechado!" << std::endl;
+    return 0;
 }
