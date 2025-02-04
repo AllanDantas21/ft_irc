1b6ae9868f50c65de30ef4bc4baf06ce994b9de0
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-04 17:06:02 -0300

    change script test/valgrind

--- test_valgrind.sh
+++ test_valgrind.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 if ! command -v valgrind &> /dev/null; then
-    echo "Valgrind não encontrado. Por favor, instale o Valgrind."
+    echo "Valgrind não está instalado. Por favor, instale o Valgrind."
     exit 1
 fi
 
-timeout 5 valgrind --leak-check=full --error-exitcode=1 ./ircserv 5555 pass > /dev/null 2>&1
-RESULTADO=$?
+valgrind --leak-check=full --log-file=valgrind.log ./ircserv 5555 pass &
 
-if [ $RESULTADO -eq 0 ]; then
+SERVER_PID=$!
+
+sleep 60
+
+kill $SERVER_PID
+
+wait $SERVER_PID
+
+if grep -q "definitely lost: 0 bytes" valgrind.log; then
     echo "Nenhum vazamento de memória detectado."
 else
     echo "Vazamento de memória detectado ou teste falhou."
+    exit 1
 fi
 
-exit $RESULTADO 
+exit 0
