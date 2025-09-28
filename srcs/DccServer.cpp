7eb5feed2e5aa5cfbd2bbcd22024115b161b74b0
thaiismeneses <thaisfmeneses@gmail.com>
2025-09-28 18:44:52 -0300

    fix: flag from fcntl

--- srcs/DccServer.cpp
+++ srcs/DccServer.cpp
@@ -42,8 +42,12 @@ int DccServer::init()
 	}
 
 	//config socket non-blocking
-	int flags = fcntl(_sockfd, F_GETFL, 0);
-	fcntl(_sockfd, F_SETFL, flags | O_NONBLOCK);
+	if (fcntl(_sockfd, F_SETFL, O_NONBLOCK) < 0)
+	{
+		std::cerr << "Error: setting socket DCC non-blocking" << std::endl;
+		close(_sockfd);
+		return (-1);
+	}
 
 	//config address
 	struct sockaddr_in addr;
