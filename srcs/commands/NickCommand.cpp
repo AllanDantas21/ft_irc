f8d6e1bb037dda076a7a416af65ac96ddafdc07f
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-10-02 21:40:18 -0300

    refactor: nick command handler

--- srcs/commands/NickCommand.cpp
+++ srcs/commands/NickCommand.cpp
@@ -1,24 +1,33 @@
 #include "../../incs/ircserv.hpp"
+#include <sstream>
+
+static const size_t MAX_NICKNAME_LENGTH = 9;
+
+
+static bool isValidNicknameStart(char c);
+static bool isValidNicknameChar(char c);
+static bool isControlChar(char c);
 
 static bool validateNickname(Server *server, const std::string &nickname, int clientFd) {
-    if (nickname.length() > 9) {
-        server->SendToClient(clientFd, "432 * " + nickname + " :Nickname too long (max 9 chars)\r\n");
+    if (nickname.length() > MAX_NICKNAME_LENGTH) {
+        std::ostringstream oss;
+        oss << "432 * " << nickname << " :Nickname too long (max " << MAX_NICKNAME_LENGTH << " chars)\r\n";
+        server->SendToClient(clientFd, oss.str());
         return false;
     }
     
-    if (!isalpha(nickname[0]) && nickname[0] != '_' && nickname[0] != '[' && nickname[0] != '{' && nickname[0] != '\\' && nickname[0] != '|' && nickname[0] != '`' && nickname[0] != '^') {
+    if (!isValidNicknameStart(nickname[0])) {
         server->SendToClient(clientFd, "432 * " + nickname + " :Nickname must start with letter or special char\r\n");
         return false;
     }
     
     for (size_t i = 0; i < nickname.length(); i++) {
-        char c = nickname[i];
-        if (c < 32 || c == 127) {
-            server->SendToClient(clientFd, "432 * " + nickname + " :Nickname contains invalid characters\r\n");
-            return false;
-        }
-        if (!isalnum(c) && c != '-' && c != '_' && c != '[' && c != ']' && c != '{' && c != '}' && c != '\\' && c != '|' && c != '`' && c != '^') {
-            server->SendToClient(clientFd, "432 * " + nickname + " :Erroneous nickname\r\n");
+        if (!isValidNicknameChar(nickname[i])) {
+            if (isControlChar(nickname[i])) {
+                server->SendToClient(clientFd, "432 * " + nickname + " :Nickname contains invalid characters\r\n");
+            } else {
+                server->SendToClient(clientFd, "432 * " + nickname + " :Erroneous nickname\r\n");
+            }
             return false;
         }
     }
@@ -46,8 +55,28 @@ void Parser::handleNick(Server *server, const std::string &nickname, int clientF
         return;
     }
     
+    std::string oldNickname = client->getNickname();
     client->SetNickname(sanitizedNickname);
-    server->SendToClient(clientFd, "Nickname set to " + sanitizedNickname + "\r\n");
+    
+    if (oldNickname.empty()) {
+        server->SendToClient(clientFd, "001 " + sanitizedNickname + " :Welcome to the IRC server\r\n");
+    } else {
+        server->SendToClient(clientFd, ":" + oldNickname + " NICK " + sanitizedNickname + "\r\n");
+    }
     
     server->CheckClientAuthentication(client);
+}
+
+static bool isValidNicknameStart(char c) {
+    return isalpha(c) || c == '_' || c == '[' || c == '{' || 
+           c == '\\' || c == '|' || c == '`' || c == '^';
+}
+
+static bool isValidNicknameChar(char c) {
+    return isalnum(c) || c == '-' || c == '_' || c == '[' || c == ']' || 
+           c == '{' || c == '}' || c == '\\' || c == '|' || c == '`' || c == '^';
+}
+
+static bool isControlChar(char c) {
+    return c < 32 || c == 127;
 }
